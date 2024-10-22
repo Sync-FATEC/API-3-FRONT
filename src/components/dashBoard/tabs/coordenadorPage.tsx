4d44fe44d9323031e586943d051eafcd3c55d68e
Yuri Braga de Lima <yuri.brgga@gmail.com>
2024-10-22 11:26:11 -0300

    Feat: Criação da página dos gráficos do Dashboard

--- src/components/dashBoard/tabs/coordenadorPage.tsx
+++ src/components/dashBoard/tabs/coordenadorPage.tsx
@@ -1,14 +1,58 @@
+import * as React from 'react';
+import { BarChart, } from '@mui/x-charts/BarChart';
+import { PieChart } from '@mui/x-charts';
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import { desktopOS, valueFormatter } from './webUsageStats';
 
-export default function CoordenadorPage() {
+
+
+export default function SimpleCharts() {
+    const theme = useTheme();
+    const smDown = useMediaQuery(theme.breakpoints.down("sm"));
+    const mdDown = useMediaQuery(theme.breakpoints.down("md"));
+
     return (
-        <div className="coordenador">
-            <div className="container-secundario">
-                <img src="/static/img/grafico01.png" alt="Welcome" /> 
-                <img src="/static/img/grafico02.png" alt="classificacao"/>
-            </div>
-            <div className="container-secundario">
-                <img src="/static/img/grafico03.svg" alt="gráfico de barra" />
-                <img src="/static/img/grafico04.png" alt="gráfico de círculo" />
-            </div>
-        </div>
-);}
+        <>
+        <Box padding={smDown ? 0 : 4} margin={smDown ? 0 : 2} display="flex" flexDirection="row">
+            <BarChart
+                xAxis={[
+                    {
+                        id: 'barCategories',
+                        data: ['bar A', 'bar B', 'bar C'],
+                        scaleType: 'band',
+                    },
+                ]}
+                series={[
+                    {
+                        data: [2, 5, 3],
+                    },
+                ]}
+                width={500}
+                height={300}
+            />
+            <BarChart
+                xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
+                series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+                width={500}
+                height={300}
+                colors={["#FF2323", "#AAAA22", "#2299AA"]} />
+                
+
+           
+        </Box>
+            
+        <Box padding={smDown ? 0 : 4} margin={smDown ? 0 : 2} display="flex" justifyContent="center">
+                <PieChart
+                     series={[
+                        {
+                          data: desktopOS,
+                          highlightScope: { fade: 'global', highlight: 'item' },
+                          faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+                          valueFormatter,
+                        },
+                      ]}
+                      height={250} />
+            </Box>
+        </>
+    );
+}
