64758487494fbffa5849c1d5bb77e678a4dbd378
Júlia Rosado <julia.rosado@fatec.sp.gov.br>
2024-11-14 17:25:14 -0300

    feat: inicio da página de cadastro e gerenciamento de bolsistas

--- src/pages/GerenciarBolsistas/gerenciarBolsistas.jsx
+++ src/pages/GerenciarBolsistas/gerenciarBolsistas.jsx
@@ -1,8 +1,41 @@
-import Sidebar from "../../components/sideBar/sideBar"
+import Sidebar from "../../components/sideBar/sideBar";
+import "./gerenciarBolsistas.css";
+
 export default function GerenciarBolsistas() {
     return (
         <>
-        <Sidebar/>
+            <Sidebar />
+            <div className= "main-conteiner-auth">
+                <div className="admin_center-header">
+                <h2>Bolsistas</h2>
+                <div className="user">
+                    <img src="/static/img/user.svg" alt="logo" />
+                    <p>Admin</p>
+                </div>
+                </div>
+                <div className="background-projects">
+                    <div className="Referencias">
+                        <p>Nome</p>
+                        <p>CPF</p>
+                        <p>RG</p>
+                        <p>E-mail</p>
+                        <p>Nacionalidade</p>
+                        <p>Visualizar</p>
+                    </div>
+                    <div className="Bolsistas Bolsistas_Responsivo">
+                        <p><label className="Referencias_Responsivo">Nome: </label></p>
+                        <p><label className="Referencias_Responsivo">CPF: </label></p>
+                        <p><label className="Referencias_Responsivo">RG: </label></p>
+                        <p><label className="Referencias_Responsivo">E-mail: </label></p>
+                        <p><label className="Referencias_Responsivo">Nacionalidade: </label></p>
+                        <img
+                            src="/static/img/pesquisar.svg"
+                            alt="Visualizar bolsistas"
+                            style={{ cursor: "pointer", transition: "transform 0.2s" }}
+                        />
+                    </div>
+                </div>
+            </div>
         </>
-    )
+    );
 }
--- src/pages/addBolsistas/addBolsistas.jsx
+++ src/pages/addBolsistas/addBolsistas.jsx
@@ -1,8 +1,69 @@
-import Sidebar from "../../components/sideBar/sideBar"
-export default function AddBolsista(){
-    return(
+import Sidebar from "../../components/sideBar/sideBar";
+import "./addBolsistas.css";
+
+
+export default function AddBolsista() {
+    return (
         <>
-        <Sidebar/>
+            <Sidebar />
+            <div className='main-conteiner-auth'>
+                <div className="admin_center-header">
+                    <h1>Cadastro de Bolsista</h1>
+                    <div className="user">
+                        <img src="/static/img/user.svg" alt="logo" />
+                        <p>Admin</p>
+                    </div>
+                </div>
+                <div>
+                    <form className="background-projects">
+                        <div>
+                            <div className="campo-projeto">
+                                <label>Nome</label>
+                                <input
+                                    type="text"
+                                    className="input"
+                                    placeholder=" "
+                                />
+                            </div>
+                            <div className="campo-projeto">
+                                <label>CPF</label>
+                                <input
+                                    type="number"
+                                    className="input"
+                                    placeholder=" "
+                                />
+                            </div>
+                            <div className="campo-projeto">
+                                <label>RG</label>
+                                <input
+                                    type="number"
+                                    className="input"
+                                    placeholder=" "
+                                />
+                            </div>
+                            <div className="campo-projeto">
+                                <label>Email</label>
+                                <input
+                                    type="email"
+                                    className="input"
+                                    placeholder=" "
+                                />
+                            </div>
+                            <div className="campo-projeto">
+                                <label>Nacionalidade</label>
+                                <input
+                                    type="text"
+                                    className="input"
+                                    placeholder=" "
+                                />
+                            </div>
+                            <div className="campo-bolsistas">
+                                <button className="btn btn-cadastrar" type="button">Cadastrar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </>
-    )
-}   
+    );
+}
